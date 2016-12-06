5f8a3d03d82c423cf196594bcc41d057f361ef5b
bbenson <bbenson@control.hotlab.pri>
2016-12-06 00:59:15 +0000

    Packer initial commit

---
+++ m2/ansible.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get -y install python3 python-dev python3-dev build-essential libssl-dev libffi-dev libxml2-dev libxslt1-dev zlib1g-dev  python3-pip
+sudo pip3 install --upgrade ansible
